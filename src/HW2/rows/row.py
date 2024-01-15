27217547f343435d1afa4c7671a22dd756b58cfc
rushil <rushilvegada@gmail.com>
2024-01-14 21:41:07 -0500

    rows folder

---
+++ src/HW2/rows/row.py
@@ -0,0 +1,42 @@
+import math
+
+
+class ROW:
+    def __init__(self, t):
+        self.cells = t
+
+    # Distance to best values (and _lower_ is _better_).
+    def d2h(self, data):
+        d, n = 0, 0
+        for col in data.cols.y:
+            n += 1
+            d += abs(col.heaven - col.norm(self.cells[col.at])) ** 2
+        return math.sqrt(d) / math.sqrt(n)
+
+    # Return the `data` (from `datas`) that I like the best
+    def likes(self, datas):
+        n, nHypotheses = 0, 0
+        for data in datas:
+            n += len(data.rows)
+            nHypotheses += 1
+
+        most, out = None, None
+        for k, data in enumerate(datas):
+            tmp = self.like(data, n, nHypotheses)
+            if most is None or tmp > most:
+                most, out = tmp, k
+        return out, most
+
+    # How much does ROW like `self`. Using logs since these
+    # numbers are going to get very small.
+    def like(self, data, n, nHypotheses):
+        prior = (len(data.rows) + the.k) / (n + the.k * nHypotheses)
+        out = math.log(prior)
+
+        for col in data.cols.x:
+            v = self.cells[col.at]
+            if v != "?":
+                inc = col.like(v, prior)
+                out += math.log(inc)
+
+        return math.exp(1) ** out
